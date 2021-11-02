c8b64bcee0994ecc0410a89d988edbc98745216c
rota99C <rotaboolean@gmail.com>
2021-11-02 18:39:08 +0100

    esecizio completo con tutti le funzionalità di base senza bonus

--- assit/main.js
+++ assit/main.js
@@ -5,19 +5,36 @@ var app = new Vue({
             'Fare i Compiti',
             'Fare la Spesa',
             'Fare il Bucato'
-        ]
+        ],
+        nothing: false,
+        short: false
+
     },
     methods: {
         removeTask(index) {
             //console.log("click");
             this.toDos.splice(index, 1)
+            if (this.toDos.length == 0) {
+                this.nothing = true;
+
+
+            }
         },
         submit() {
             //console.log("click");
             let yourTask = document.querySelector(".input1").value
-            console.log(yourTask);
-            this.toDos.push(yourTask)
+            // console.log(yourTask);
+
+            if (yourTask.length < 4) {
+                //console.log("errore");
+                this.short = true;
+
+            } else {
+                this.toDos.push(yourTask)
+            }
         }
+
+
 
 
     }
