55066467f58bdf17d8f0c6ffd0b8b262c02210ba
rota99C <rotaboolean@gmail.com>
2021-11-02 12:30:07 +0100

    creato parte superiore con logo e due pulsanti e fatto un array di "cose da fare"

---
+++ assit/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#container',
+    data: {
+        toDos: [
+            'Fare i Compiti',
+            'Fare i Spesa',
+            'Fare i Bucato'
+
+        ]
+
+    }
+})
